81e98d4ffed7fd43b8606f4a7377240e79ca8cfa
Shane Smith <voodoogq@gmail.com>
2017-12-24 19:28:03 -0600

    Fleshing out PlayerConnectionSpec

---
+++ spec/lib/player_connection_spec.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+require "spec_helper"
+
+RSpec.describe PlayerConnection, type: [:socket, :connection] do
+  include_context "socket"
+
+  let(:subject) { described_class }
+  let(:signature) { rand(1..100) }
+
+  describe "post_init" do
+    it "logs a message to stdout" do
+      spec_socket_server do
+        # No need to call post_init directly as will happen after init
+        expect{ subject.new(signature) }.to output(
+          "-- someone connected to the echo server!\n"
+        ).to_stdout
+      end
+    end
+  end
+
+  describe "process_player" do
+    it "adds the player object to the connection" do
+      spec_socket_server do
+        obj = subject.new(signature)
+        player = build(:player)
+
+        obj.process_player(player)
+        expect(obj.player).to eq(player)
+      end
+    end
+
+    it "adds itself to the passed in player" do
+      spec_socket_server do
+        obj = subject.new(signature)
+        player = build(:player)
+
+        obj.process_player(player)
+        expect(player.connection).to eq(obj)
+      end
+    end
+
+    it "assigns a new CommandParser object to the connection and adds the " \
+      "player to the parser" do
+      spec_socket_server do
+        obj = subject.new(signature)
+        player = build(:player)
+
+        obj.process_player(player)
+        expect(obj.command_parser).to be_a(CommandParser)
+        expect(obj.command_parser.player).to eq(player)
+      end
+    end
+
+    it "calls to the 'add_connection_to_pool' method of the Game instance" do
+      spec_socket_server do
+        game = Game.instance
+        obj = subject.new(signature)
+        player = build(:player)
+
+        expect(game).to receive(:add_connection_to_pool).with(obj)
+        obj.process_player(player)
+      end
+    end
+  end
+
+  describe "receive_data" do
+    it "calls to the creation_processor when the new_character flag is set" do
+      spec_socket_server do
+        obj = subject.new(signature)
+        obj.new_character = true
+
+        expect(obj.creation_processor).to receive(:call).with("foo")
+        obj.receive_data("foo")
+      end
+    end
+  end
+
+  describe "close_connection" do
+
+  end
+
+  describe "unbind" do
+
+  end
+end
