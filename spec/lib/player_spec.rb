f214d49915aeb1b5530a160f5c8407d3ebdd9df1
Shane Smith <voodoogq@gmail.com>
2017-12-24 23:07:28 -0600

    PlayerSpecs

--- spec/lib/player_spec.rb
+++ spec/lib/player_spec.rb
@@ -4,4 +4,18 @@ require "spec_helper"
 RSpec.describe Player, type: [:model] do
   let(:subject) { described_class }
 
+  describe "instance_methods" do
+    describe "method_missing" do
+      it "passes unknown methods to the connection" do
+        player = build(:player)
+        player.instance_variable_set(
+          :@connection, instance_double("PlayerConneciton")
+        )
+
+        expect(player.connection).to receive(:foo)
+        player.foo
+      end
+    end
+  end
+
 end
