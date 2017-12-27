703d6b231fcd53eaa25d3fd69e3ad94916403269
Shane Smith <voodoogq@gmail.com>
2017-12-26 20:52:34 -0600

    Adding validators to Room

--- lib/models/room.rb
+++ lib/models/room.rb
@@ -2,6 +2,16 @@
 require "active_record"
 
 class Room < ActiveRecord::Base
-  belongs_to :area
-  belongs_to :sector
+  validate :unique_coordinates_by_area
+  validates :x_coord, presence: true
+  validates :y_coord, presence: true
+
+  belongs_to :area, required: true
+  belongs_to :sector, required: true
+
+  def unique_coordinates_by_area
+    if area.rooms.where("x_coord = ? AND y_coord = ?", x_coord, y_coord).any?
+      errors.add(:x_coord, "and Y coord already exist in Area (#{area.name})")
+    end
+  end
 end
