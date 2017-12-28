355cc81f39e2557a7a7984e3a3498bb0e4063db9
Shane Smith <voodoogq@gmail.com>
2017-12-28 15:35:58 -0600

    Adding location data to the map

--- lib/commands/look.rb
+++ lib/commands/look.rb
@@ -3,10 +3,19 @@ require_relative "command_base"
 
 module Commands
   class Look < CommandBase
+    attr_reader :room, :area
+
     def call(*args)
-      room = @initiator.room
-      area = room.area
+      @room = @initiator.room
+      @area = room.area
       @initiator.send_data(area.map.show(room, 11, 25))
+      @initiator.send_data(location_data)
+    end
+
+    private
+
+    def location_data
+      "(#{@area.name}) [#{@room.x_coord}, #{@room.y_coord}]"
     end
   end
 end
