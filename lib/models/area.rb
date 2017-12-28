eac902e07a2a7f5c78727d0bc8861db2804187a1
Shane Smith <voodoogq@gmail.com>
2017-12-27 21:54:13 -0600

    Adding dimension to the Area model

--- lib/models/area.rb
+++ lib/models/area.rb
@@ -16,4 +16,22 @@ class Area < ActiveRecord::Base
   validates :dimension, presence: true, numericality: {
     odd: true, greater_than_or_equal_to: 15, less_than_or_equal_to: 101
   }
+
+  after_create :create_rooms
+
+  def create_rooms
+    grid_coord_index = -(dimension.divmod(2).first)
+
+    dimension.times do |y_index|
+      dimension.times do |x_index|
+        Room.create!(
+          override: true,
+          y_coord: grid_coord_index + y_index,
+          x_coord: grid_coord_index + x_index,
+          area: self,
+          sector: Sector.find(1)
+        )
+      end
+    end
+  end
 end
